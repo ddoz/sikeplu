8bdd1cda9404b7327e07359dfa0d8581fbbf65d4
Adam Japal <adam.japal@gmail.com>
2022-02-26 09:07:50 +0700

    a

---
+++ application/controllers/File.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class File extends CI_Controller {
+
+	public function __construct() {
+        parent::__construct();
+	}
+	
+	public function download()
+	{
+        $this->load->helper('download');
+        $id = $this->uri->segment(3);
+        $file = $this->db->get_where('order_media',array('md5(id)' => $id));
+        if($file->num_rows()>0) {
+            $f = $file->row();
+            if($f->surat_order!='') {
+                force_download(base_url().'/'.$file->surat_order, NULL);
+            }else {
+                echo "Surat tidak tersedia. Silahkan <a href='".base_url()."welcome'> Login </a> untuk melihat data lengkap pada menu Order Media.";
+            }
+        }
+    }
+    
+}
